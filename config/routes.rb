b6ce86e81bf79127d7b678193e9d8494d99113d5
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-14 10:19:28 -0400

    new api/v1 folders

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :carts
-  resources :products
-  resources :customers
-  resources :farmers
- 
+  
+  namespace :api do
+    namespace :v1 do
+      resources :carts
+      resources :products
+      resources :customers
+      resources :farmers
+    end  
+  end
 end
