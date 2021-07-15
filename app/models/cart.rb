79311f2066470337aec2db1bee1a7c8cd7f5348c
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-15 08:47:00 -0400

    added more validations and connecting product add with cart balance

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,10 @@
 class Cart < ApplicationRecord
     belongs_to :customer
     has_and_belongs_to_many :products
+    validates :total, :numericality => { :greater_than_or_equal_to => 0 }
+
+    def update_cart(product)
+        self.total = self.total + product.price
+        self.save
+    end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :farmer
     has_and_belongs_to_many :carts
-
+    
+    validates :price, :numericality => { :greater_than_or_equal_to => 0 }
     validates :name, :variety, :price, presence: true
 end
