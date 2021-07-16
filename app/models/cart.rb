8902f7d0bff926350de43ef441b662dc3f8ead3c
Seth Blanchard <nsethblanchard@gmail.com>
2021-07-16 11:30:02 -0400

    fixed relationships in models

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,12 +1,12 @@
 class Cart < ApplicationRecord
     belongs_to :customer
-    has_many :products
     belongs_to :product
     
-    validates :total, :numericality => { :greater_than_or_equal_to => 0 }
+    validates :quantity, :numericality => { :greater_than_or_equal_to => 0 }
 
+    # this needs work
     def update_cart(product)
-        self.total = self.total + product.price
+        self.quantity = self.quantity + product.price
         self.save
     end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,7 @@
 class Product < ApplicationRecord
     belongs_to :farmer
-    has_many :carts
-    belongs_to :cart
+    has_many :customers, through: :carts
+    
     
     validates :price, :numericality => { :greater_than_or_equal_to => 0 }
     validates :name, :variety, :price, presence: true
