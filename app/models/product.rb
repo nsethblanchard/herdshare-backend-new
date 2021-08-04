fe0fa3429dfbfce8a701bf4b973d1f7f3c135dde
Seth Blanchard <nsethblanchard@gmail.com>
2021-08-04 10:10:51 -0400

    removed validation from product controller

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,5 @@ class Product < ApplicationRecord
     belongs_to :farmer
     has_many :customers, through: :carts
     
-    
-    validates :price, :numericality => { :greater_than_or_equal_to => 0 }
     validates :name, :variety, :price, presence: true
 end
